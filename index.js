c53988099ae54b95fe4e1734ae013455bafbb823
Thomas Luo <tluo9713@gmail.com>
2018-12-07 01:20:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+<<<<<<< HEAD
 var conversation = `Let\'s talk about ${topic}.`;
+=======
+var conversation = `Let\`s talk about ${topic}`;
+>>>>>>> 7faf9ae17d68f1c24f76bde02e93f6c2ac1f25ca
 
